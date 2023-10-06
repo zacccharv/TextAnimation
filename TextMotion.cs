91946b2aa878fc6631249a0552698f7420346628
Zaccharie Charvolin <zacchien1@gmail.com>
2023-10-06 00:28:51 -0400

    Color Shift

--- TextMotion.cs
+++ TextMotion.cs
@@ -1,25 +1,72 @@
 using Godot;
 using System;
 
+public enum FunctionSelection
+{
+	Function1,
+	Function2
+}
+
 public partial class TextMotion : RichTextLabel
 {
-	float t = 0;
-	Vector2 Center;
-	// Called when the node enters the scene tree for the first time.
+	[Export] FunctionSelection functionSelection;
+	[Export] float shiftSpeed = .001f;
+	float _t = 0;
+	Vector2 _Center;
 	public override void _Ready()
 	{
-		Center = GetWindow().Size / 2;
+		_Center = GetWindow().Size / 2;
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
+    {
+        _t += (float)delta;
+
+		if (functionSelection == FunctionSelection.Function1)
+        {
+            FirstMotionFunction();
+        }
+		else if (functionSelection == FunctionSelection.Function2)
+		{
+			SecondMotionFunction();
+		}
+    }
+
+    void FirstMotionFunction()
+    {        
+        Position = new Vector2((Mathf.Sin(_t) * 200) + _Center.X - (Size.X / 2), (Mathf.Cos(_t) * 200) + _Center.Y - (Size.Y / 2));
+
+        Rotation = _t - Mathf.Sin(_t);
+
+        Color outlineColor = RainbowShift(shiftSpeed, "theme_override_colors/font_outline_color");
+        Set("theme_override_colors/font_outline_color", outlineColor);
+
+        Color defaultColor = RainbowShift(shiftSpeed+(shiftSpeed/15), "theme_override_colors/default_color");
+        Set("theme_override_colors/default_color", defaultColor);
+    }
+	void SecondMotionFunction()
 	{
-		t += (float)delta;
-		Position = new Vector2((Mathf.Sin(t) * 100) + Center.X - (Size.X/2), (Mathf.Cos(t) * 100) + Center.Y - (Size.Y/2));
-		Rotation = t;
+        Position = new Vector2((Mathf.Sin(_t) * 200) + _Center.X - (Size.X / 2), (Mathf.Cos(_t) * 200) + (_Center.Y - 90) - (Size.Y / 2));
 
-		Color col = (Color)Get("theme_override_colors/default_color");
+        Rotation = _t + Mathf.Sin(_t);
 
-		Set("theme_override_colors/default_color", new Color(1, 1, 1, 1));
+        Color outlineColor = RainbowShift(shiftSpeed+(shiftSpeed/12), "theme_override_colors/font_outline_color");
+        Set("theme_override_colors/font_outline_color", outlineColor);
+
+        Color defaultColor = RainbowShift(shiftSpeed, "theme_override_colors/default_color");
+        Set("theme_override_colors/default_color", defaultColor);
 	}
+
+    Color RainbowShift(float shiftSpeed, string colorNodePath)
+    {
+        Vector3 HSV = new Vector3();
+
+        Color col = (Color)Get(colorNodePath);
+
+        col.ToHsv(out HSV.X, out HSV.Y, out HSV.Z);
+
+        Color newColor = Color.FromHsv(HSV.X + shiftSpeed, HSV.Y, HSV.Z);
+
+        return newColor;
+    }
+
 }
